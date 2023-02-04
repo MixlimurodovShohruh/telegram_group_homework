eeb854018d2f0f033f73afcd3fa2742ff48a5e3d
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-02-04 11:37:15 +0500

    finish

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -10,4 +10,13 @@ def find_number_of_messages(data: dict)->int:
         int: Total number of messages.
     
     """
-    return
+    sum=0
+    for i in data['messages']:
+        if i['type']=='message':
+
+            sum+=1
+    return sum
+
+path='data/result.json'
+
+print(find_number_of_messages(read_data(path)))
